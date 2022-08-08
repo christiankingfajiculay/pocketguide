717a7770853f3e90cc9ac5cc4e4aee04f7d62d99
king <ckffajiculay@donbosco.edu.ph>
2022-08-08 15:45:30 +0800

    initial commit

---
+++ post/index.php
@@ -0,0 +1,17 @@
+<?php
+	
+?>
+
+
+<form action='post/insert.php' method='POST'>
+	<label>Subject :</label>
+	<input type='text' name='a_subject' placeholder='Subject'/><br/>
+		<label>Content :</label>
+	<input type='text' name='a_content' placeholder='Content'/><br/>
+		<label>Author : </label>
+	<input type='text' name='a_author' placeholder='Author'/><br/>
+		<label>Date Posted : </label>
+	<input type='text' name='date_posted' placeholder='Date posted'/><br/>
+	<input type='submit' value='Post'/>
+
+</form>
---
+++ post/insert.php
@@ -0,0 +1,23 @@
+<?php
+	include('../settings.php');
+	include('../queries/post.qry');	
+	
+	$a_subject = $_POST['a_subject'];
+	$a_content = $_POST['a_content'];
+	$a_author = $_POST['a_author'];
+	$date_posted = $_POST['date_posted'];
+	
+
+	$announce = new Post;
+	$make_announcement = $announce->insert_post(
+		$pdo,
+		$date_posted,
+		$a_author, 
+		$a_subject, 
+		$a_content
+	
+	);
+	
+	
+	header('Location: ../home.php');
+?>
